adaaaf7d231bd73e3f1c3b46f254c0d37aeee111
Tim Krones <tim@opencraft.com>
2016-01-14 11:18:40 +0100

    Fix Travis build.

--- install_test_deps.sh
+++ install_test_deps.sh
@@ -3,4 +3,4 @@
 pip install -e git://github.com/edx/xblock-sdk.git@4e8e713e7dd886b8d2eb66b5001216b66b9af81a#egg=xblock-sdk
 cd $VIRTUAL_ENV/src/xblock-sdk/ && pip install -r requirements/base.txt \
                                 && pip install -r requirements/test.txt && cd -
-python setup.py develop
+pip install -r requirements.txt
