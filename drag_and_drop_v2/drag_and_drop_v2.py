afd30bde942a5b1f45cd0158b9256b6278da8aaf
Matt Drayer <mattdrayer@edx.org>
2016-02-17 16:21:57 -0500

    mattdrayer/xblock-translations: Initial attempt

--- drag_and_drop_v2/drag_and_drop_v2.py
+++ drag_and_drop_v2/drag_and_drop_v2.py
@@ -27,6 +27,7 @@ loader = ResourceLoader(__name__)
 # Classes ###########################################################
 
 @XBlock.wants('settings')
+@XBlock.needs('i18n')
 class DragAndDropBlock(XBlock, XBlockWithSettingsMixin, ThemableXBlockMixin):
     """
     XBlock that implements a friendly Drag-and-Drop problem
@@ -107,8 +108,13 @@ class DragAndDropBlock(XBlock, XBlockWithSettingsMixin, ThemableXBlockMixin):
     has_score = True
 
     def _(self, text):
-        """ Translate text """
-        return self.runtime.service(self, "i18n").ugettext(text)
+        """
+        Hand the translation work off to the runtime's I18N service
+        """
+        runtime_service = self.runtime.service(self, "i18n")
+        runtime_ugettext = runtime_service.ugettext
+        response = runtime_ugettext(text)
+        return response
 
     @XBlock.supports("multi_device")  # Enable this block for use in the mobile app via webview
     def student_view(self, context):
@@ -146,9 +152,16 @@ class DragAndDropBlock(XBlock, XBlockWithSettingsMixin, ThemableXBlockMixin):
         and feedback.
         """
 
-        def items_without_answers():
+        def items_without_answers(self):
+            """
+            Internal helper to construct the set of unanswered items and their fields for the XBlock
+            """
             items = copy.deepcopy(self.data.get('items', ''))
             for item in items:
+                if 'displayName' in item:
+                    item['displayName'] = self._(item['displayName'])
+                if 'correct' in item:
+                    item['correct'] = self,_(item['correct'])
                 del item['feedback']
                 del item['zone']
                 item['inputOptions'] = 'inputOptions' in item
@@ -160,16 +173,16 @@ class DragAndDropBlock(XBlock, XBlockWithSettingsMixin, ThemableXBlockMixin):
             "url_name": getattr(self, 'url_name', ''),
             "display_zone_labels": self.data.get('displayLabels', False),
             "display_zone_borders": self.data.get('displayBorders', False),
-            "items": items_without_answers(),
-            "title": self.display_name,
+            "items": items_without_answers(self),
+            "title": self._(self.display_name),
             "show_title": self.show_title,
-            "problem_text": self.question_text,
+            "problem_text": self._(self.question_text),
             "show_problem_header": self.show_question_header,
             "target_img_expanded_url": self.target_img_expanded_url,
-            "target_img_description": self.target_img_description,
+            "target_img_description": self._(self.target_img_description),
             "item_background_color": self.item_background_color or None,
             "item_text_color": self.item_text_color or None,
-            "initial_feedback": self.data['feedback']['start'],
+            "initial_feedback": self._(self.data['feedback']['start']),
             # final feedback (data.feedback.finish) is not included - it may give away answers.
         }
 
@@ -411,6 +424,7 @@ class DragAndDropBlock(XBlock, XBlockWithSettingsMixin, ThemableXBlockMixin):
         zones = []
         for zone in self.data.get('zones', []):
             zone = zone.copy()
+            zone['title'] = self._(zone['title'])
             if "uid" not in zone:
                 zone["uid"] = zone.get("title")  # Older versions used title as the zone UID
             # Remove old, now-unused zone attributes, if present:
