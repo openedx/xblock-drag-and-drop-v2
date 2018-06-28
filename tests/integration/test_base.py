0cf9eca2c9cc37077300f3288c2cf3eacc69cc9a
Saqib <msaqib52@gmail.com>
2018-06-28 11:56:26 +0500

    add tests

--- tests/integration/test_base.py
+++ tests/integration/test_base.py
@@ -422,9 +422,17 @@ class InteractionTestBase(object):
         zone.send_keys(action_key)
 
     def assert_item_grabbed(self, item):
+        drag_container = item.find_element_by_xpath(
+            "./ancestor::div[contains(concat(' ', @class, ' '), ' drag-container ')][1]"
+        )
+        self.assertIn("dragging", drag_container.get_attribute('class').split(' '))
         self.assertEqual(item.get_attribute('aria-grabbed'), 'true')
 
     def assert_item_not_grabbed(self, item):
+        drag_container = item.find_element_by_xpath(
+            "./ancestor::div[contains(concat(' ', @class, ' '), ' drag-container ')][1]"
+        )
+        self.assertNotIn("dragging", drag_container.get_attribute('class').split(' '))
         self.assertEqual(item.get_attribute('aria-grabbed'), 'false')
 
     def assert_placed_item(self, item_value, zone_title, assessment_mode=False):
