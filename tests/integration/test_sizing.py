397c58234f6e0973173c6839bed2beada6549f1e
jawad-khan <jawadkhan444@gmail.com>
2018-07-23 14:10:30 +0500

    fixed failing tests

--- tests/integration/test_sizing.py
+++ tests/integration/test_sizing.py
@@ -487,9 +487,7 @@ class FreeSizingTests(SizingTests, FreeSizingInteractionTestBase):
 
         # Test that the item bank maintains its original size.
         self.assertEqual(item_bank.size["width"], item_bank_width)
-
-        # V4 todo: pass this test after restyling of item bank in MCKIN-7971
-        # self.assertEqual(item_bank.size["height"], item_bank_height)
+        self.assertEqual(item_bank.size["height"], item_bank_height)
 
 
 class AlignedSizingTests(SizingTests):
