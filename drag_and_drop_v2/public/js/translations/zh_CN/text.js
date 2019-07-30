6040da49ebcfb5361764e5b5d05e776b662c004d
Omar Al-Ithawi <i@omardo.com>
2019-07-30 16:08:27 +0300

    Update translations (autogenerated message)

--- drag_and_drop_v2/public/js/translations/zh_CN/text.js
+++ drag_and_drop_v2/public/js/translations/zh_CN/text.js
@@ -10,7 +10,7 @@
 
   
   django.pluralidx = function(n) {
-    var v=(n > 1);
+    var v=0;
     if (typeof(v) == 'boolean') {
       return v ? 1 : 0;
     } else {
@@ -22,27 +22,6 @@
   /* gettext library */
 
   django.catalog = django.catalog || {};
-  
-  var newcatalog = {
-    "An isosceles triangle with three layers of similar height. It is shown upright, so the widest layer is located at the bottom, and the narrowest layer is located at the top.": "Un triangle isoc\u00e8le avec trois niveaux de m\u00eame hauteur. Il est montr\u00e9 la base en bas, et donc le plus large niveau est situ\u00e9 \u00e0 la base, alors que le niveau le plus \u00e9troit est situ\u00e9 au sommet.", 
-    "Drag and Drop": "Glisser D\u00e9poser", 
-    "Drag the items onto the image above.": "Glissez cet \u00e9l\u00e9ment sur l'image ci-dessus.", 
-    "Goes anywhere": "Va n'importe o\u00f9", 
-    "Goes to the top": "Va au sommet", 
-    "Good work! You have completed this drag and drop problem.": "Bon travail! Vous avez compl\u00e9t\u00e9 cet exercice de glisser-d\u00e9poser.", 
-    "No, this item does not belong here. Try again.": "Non, cet \u00e9l\u00e9ment n'appartient pas l\u00e0. Essayez encore.", 
-    "Of course it goes here! It goes anywhere!": "Bien s\u00fbr qu'il va l\u00e0\u00a0! Il peut aller n'importe o\u00f9\u00a0!", 
-    "The Bottom Zone": "La zone du bas", 
-    "The Middle Zone": "La zone du milieu", 
-    "The Top Zone": "La zone du haut", 
-    "Use this zone to associate an item with the middle layer of the triangle.": "Utilisez cette zone pour y associer un \u00e9l\u00e9ment au niveau au milieu du triangle.", 
-    "Use this zone to associate an item with the top layer of the triangle.": "Utilisez cette zone pour y associer un \u00e9l\u00e9ment au niveau au sommet du triangle.", 
-    "You silly, there are no zones for this one.": "Il n'y a pas de zone pour celui-l\u00e0.", 
-    "Your highest score is {score}": "Votre score le plus \u00e9lev\u00e9 est {score}"
-  };
-  for (var key in newcatalog) {
-    django.catalog[key] = newcatalog[key];
-  }
   
 
   if (!django.jsi18n_initialized) {
@@ -94,37 +73,43 @@
     /* formatting library */
 
     django.formats = {
-    "DATETIME_FORMAT": "j F Y H:i", 
+    "DATETIME_FORMAT": "N j, Y, P", 
     "DATETIME_INPUT_FORMATS": [
-      "%d/%m/%Y %H:%M:%S", 
-      "%d/%m/%Y %H:%M:%S.%f", 
-      "%d/%m/%Y %H:%M", 
-      "%d/%m/%Y", 
-      "%d.%m.%Y %H:%M:%S", 
-      "%d.%m.%Y %H:%M:%S.%f", 
-      "%d.%m.%Y %H:%M", 
-      "%d.%m.%Y", 
       "%Y-%m-%d %H:%M:%S", 
       "%Y-%m-%d %H:%M:%S.%f", 
       "%Y-%m-%d %H:%M", 
-      "%Y-%m-%d"
+      "%Y-%m-%d", 
+      "%m/%d/%Y %H:%M:%S", 
+      "%m/%d/%Y %H:%M:%S.%f", 
+      "%m/%d/%Y %H:%M", 
+      "%m/%d/%Y", 
+      "%m/%d/%y %H:%M:%S", 
+      "%m/%d/%y %H:%M:%S.%f", 
+      "%m/%d/%y %H:%M", 
+      "%m/%d/%y"
     ], 
-    "DATE_FORMAT": "j F Y", 
+    "DATE_FORMAT": "N j, Y", 
     "DATE_INPUT_FORMATS": [
-      "%d/%m/%Y", 
-      "%d/%m/%y", 
-      "%d.%m.%Y", 
-      "%d.%m.%y", 
-      "%Y-%m-%d"
+      "%Y-%m-%d", 
+      "%m/%d/%Y", 
+      "%m/%d/%y", 
+      "%b %d %Y", 
+      "%b %d, %Y", 
+      "%d %b %Y", 
+      "%d %b, %Y", 
+      "%B %d %Y", 
+      "%B %d, %Y", 
+      "%d %B %Y", 
+      "%d %B, %Y"
     ], 
-    "DECIMAL_SEPARATOR": ",", 
-    "FIRST_DAY_OF_WEEK": "1", 
-    "MONTH_DAY_FORMAT": "j F", 
-    "NUMBER_GROUPING": "3", 
-    "SHORT_DATETIME_FORMAT": "j N Y H:i", 
-    "SHORT_DATE_FORMAT": "j N Y", 
-    "THOUSAND_SEPARATOR": "\u00a0", 
-    "TIME_FORMAT": "H:i", 
+    "DECIMAL_SEPARATOR": ".", 
+    "FIRST_DAY_OF_WEEK": "0", 
+    "MONTH_DAY_FORMAT": "F j", 
+    "NUMBER_GROUPING": "0", 
+    "SHORT_DATETIME_FORMAT": "m/d/Y P", 
+    "SHORT_DATE_FORMAT": "m/d/Y", 
+    "THOUSAND_SEPARATOR": ",", 
+    "TIME_FORMAT": "P", 
     "TIME_INPUT_FORMATS": [
       "%H:%M:%S", 
       "%H:%M:%S.%f", 
