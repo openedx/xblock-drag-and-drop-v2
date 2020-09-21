48bbfeb86fa61c853a2fb112f6b7e0b72c4b2e59
musmanmalik <m.usman@arbisoft.com>
2020-09-21 16:36:33 +0500

    missing translations added

--- drag_and_drop_v2/public/js/translations/es_419/text.js
+++ drag_and_drop_v2/public/js/translations/es_419/text.js
@@ -25,8 +25,17 @@
   
   var newcatalog = {
     "Feedback": "Realimentaci\u00f3n", 
+    "Keyboard Help": "\u952e\u76d8\u8bf4\u660e", 
     "Loading drag and drop problem.": "Cargando problema de arrastrar y soltar.", 
-    "Reset": "Restablecer"
+    "Navigate using TAB and SHIFT+TAB to the appropriate dropzone and press CTRL+M once more to drop it here.": "Para navegar, utilice las teclas Tab y May\u00fas+Tab hasta el panel Colocar zonas correspondiente, y luego presione las teclas CTRL+M una vez m\u00e1s para soltar el elemento.", 
+    "Press CTRL+M to select a draggable item (effectively picking it up).": "Para seleccionar un elemento que se puede arrastrar, presione las teclas CTRL+M. (Esta es una forma efectiva de selecci\u00f3n).", 
+    "Press ESC if you want to cancel the drop operation (for example, to select a different item).": "Si desea cancelar la operaci\u00f3n de colocar, presione la tecla Esc. (Por ejemplo, para seleccionar otro elemento).", 
+    "Problem": "Problema", 
+    "Reset": "Restablecer", 
+    "TAB back to the list of draggable items and repeat this process until all of the draggable items have been placed on their respective dropzones.": "Presione las teclas May\u00fas+Tab para desplazarse hacia atr\u00e1s por la lista de elementos que se pueden seleccionar; luego, repita el proceso hasta que todos los elementos que se pueden seleccionar se hayan colocado en sus respectivos paneles Colocar zonas.", 
+    "Use only TAB and SHIFT+TAB to navigate between draggable items and drop zones.": "Para navegar entre los elementos que se pueden arrastrar y el panel Colocar zonas, utilice solo las teclas Tab y May\u00fas+Tab.", 
+    "You can complete this problem using only your keyboard by following the guidance below:": "Puede completar este problema utilizando solo el teclado. Para ello, siga las indicaciones a continuaci\u00f3n:", 
+    "You cannot add any more items to this zone.": "No se pueden agregar m\u00e1s elementos a esta zona."
   };
   for (var key in newcatalog) {
     django.catalog[key] = newcatalog[key];
