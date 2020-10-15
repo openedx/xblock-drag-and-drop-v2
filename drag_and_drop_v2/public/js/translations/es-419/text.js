dee8bcb9110910d3cc21d74bc01cf79199d91dd5
Wasif ur Rehman <wasif.rehman@arbisoft.com>
2020-10-15 16:54:15 +0500

    MCKIN-26123 Platform Issues - Translations

--- drag_and_drop_v2/public/js/translations/es-419/text.js
+++ drag_and_drop_v2/public/js/translations/es-419/text.js
@@ -2,13 +2,13 @@
             (function(global){
                 var DragAndDropI18N = {
                   init: function() {
-                    
+
 
 (function(globals) {
 
   var django = globals.django || (globals.django = {});
 
-  
+
   django.pluralidx = function(n) {
     var v=(n != 1);
     if (typeof(v) == 'boolean') {
@@ -17,21 +17,30 @@
       return v;
     }
   };
-  
+
 
   /* gettext library */
 
   django.catalog = django.catalog || {};
-  
+
   var newcatalog = {
-    "Feedback": "Realimentaci\u00f3n", 
-    "Loading drag and drop problem.": "Cargando problema de arrastrar y soltar.", 
-    "Reset": "Restablecer"
+    "Feedback": "Realimentaci\u00f3n",
+    "Keyboard Help": "Ayuda del teclado",
+    "Loading drag and drop problem.": "Cargando problema de arrastrar y soltar.",
+    "Navigate using TAB and SHIFT+TAB to the appropriate dropzone and press CTRL+M once more to drop it here.": "Para navegar, utilice las teclas Tab y May\u00fas+Tab hasta el panel Colocar zonas correspondiente, y luego presione las teclas CTRL+M una vez m\u00e1s para soltar el elemento.",
+    "Press CTRL+M to select a draggable item (effectively picking it up).": "Para seleccionar un elemento que se puede arrastrar, presione las teclas CTRL+M. (Esta es una forma efectiva de selecci\u00f3n).",
+    "Press ESC if you want to cancel the drop operation (for example, to select a different item).": "Si desea cancelar la operaci\u00f3n de colocar, presione la tecla Esc. (Por ejemplo, para seleccionar otro elemento).",
+    "Problem": "Problema",
+    "Reset": "Restablecer",
+    "TAB back to the list of draggable items and repeat this process until all of the draggable items have been placed on their respective dropzones.": "Presione las teclas May\u00fas+Tab para desplazarse hacia atr\u00e1s por la lista de elementos que se pueden seleccionar; luego, repita el proceso hasta que todos los elementos que se pueden seleccionar se hayan colocado en sus respectivos paneles Colocar zonas.",
+    "Use only TAB and SHIFT+TAB to navigate between draggable items and drop zones.": "Para navegar entre los elementos que se pueden arrastrar y el panel Colocar zonas, utilice solo las teclas Tab y May\u00fas+Tab.",
+    "You can complete this problem using only your keyboard by following the guidance below:": "Puede completar este problema utilizando solo el teclado. Para ello, siga las indicaciones a continuaci\u00f3n:",
+    "You cannot add any more items to this zone.": "No se pueden agregar m\u00e1s elementos a esta zona."
   };
   for (var key in newcatalog) {
     django.catalog[key] = newcatalog[key];
   }
-  
+
 
   if (!django.jsi18n_initialized) {
     django.gettext = function(msgid) {
@@ -82,38 +91,38 @@
     /* formatting library */
 
     django.formats = {
-    "DATETIME_FORMAT": "j \\d\\e F \\d\\e Y \\a \\l\\a\\s H:i", 
+    "DATETIME_FORMAT": "j \\d\\e F \\d\\e Y \\a \\l\\a\\s H:i",
     "DATETIME_INPUT_FORMATS": [
-      "%d/%m/%Y %H:%M:%S", 
-      "%d/%m/%Y %H:%M:%S.%f", 
-      "%d/%m/%Y %H:%M", 
-      "%d/%m/%y %H:%M:%S", 
-      "%d/%m/%y %H:%M:%S.%f", 
-      "%d/%m/%y %H:%M", 
-      "%Y-%m-%d %H:%M:%S", 
-      "%Y-%m-%d %H:%M:%S.%f", 
-      "%Y-%m-%d %H:%M", 
+      "%d/%m/%Y %H:%M:%S",
+      "%d/%m/%Y %H:%M:%S.%f",
+      "%d/%m/%Y %H:%M",
+      "%d/%m/%y %H:%M:%S",
+      "%d/%m/%y %H:%M:%S.%f",
+      "%d/%m/%y %H:%M",
+      "%Y-%m-%d %H:%M:%S",
+      "%Y-%m-%d %H:%M:%S.%f",
+      "%Y-%m-%d %H:%M",
       "%Y-%m-%d"
-    ], 
-    "DATE_FORMAT": "j \\d\\e F \\d\\e Y", 
+    ],
+    "DATE_FORMAT": "j \\d\\e F \\d\\e Y",
     "DATE_INPUT_FORMATS": [
-      "%d/%m/%Y", 
-      "%d/%m/%y", 
+      "%d/%m/%Y",
+      "%d/%m/%y",
       "%Y-%m-%d"
-    ], 
-    "DECIMAL_SEPARATOR": ",", 
-    "FIRST_DAY_OF_WEEK": "1", 
-    "MONTH_DAY_FORMAT": "j \\d\\e F", 
-    "NUMBER_GROUPING": "3", 
-    "SHORT_DATETIME_FORMAT": "d/m/Y H:i", 
-    "SHORT_DATE_FORMAT": "d/m/Y", 
-    "THOUSAND_SEPARATOR": ".", 
-    "TIME_FORMAT": "H:i", 
+    ],
+    "DECIMAL_SEPARATOR": ",",
+    "FIRST_DAY_OF_WEEK": "1",
+    "MONTH_DAY_FORMAT": "j \\d\\e F",
+    "NUMBER_GROUPING": "3",
+    "SHORT_DATETIME_FORMAT": "d/m/Y H:i",
+    "SHORT_DATE_FORMAT": "d/m/Y",
+    "THOUSAND_SEPARATOR": ".",
+    "TIME_FORMAT": "H:i",
     "TIME_INPUT_FORMATS": [
-      "%H:%M:%S", 
-      "%H:%M:%S.%f", 
+      "%H:%M:%S",
+      "%H:%M:%S.%f",
       "%H:%M"
-    ], 
+    ],
     "YEAR_MONTH_FORMAT": "F \\d\\e Y"
   };
 
@@ -147,4 +156,3 @@
                 DragAndDropI18N.init();
                 global.DragAndDropI18N = DragAndDropI18N;
             }(this));
-        
