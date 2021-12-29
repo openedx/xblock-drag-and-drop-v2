25d381a8aaf183a70e01aa53d73603379f1028d4
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2021-12-29 18:20:59 +0500

    fix: try

--- install_test_deps.sh
+++ install_test_deps.sh
@@ -1,6 +1,7 @@
 # Installs xblock-sdk and dependencies needed to run the tests suite.
 # Run this script inside a fresh virtual environment.
+pip install -r requirements.txt
 rm -rf src/xblock-sdk
 pip install -e git://github.com/edx/xblock-sdk.git@4d0027a040aec1be1a22dc9a4b8fc667bba0ddba#egg=xblock-sdk
-cd src/xblock-sdk/ && ls -la && pip install -r requirements/base.txt && pip install -r requirements/test.txt && cd - && ls -la
-pip install -r requirements.txt
+cd src/xblock-sdk/ && ls -la && pip install -r requirements/base.txt && pip install -r requirements/test.txt 
+
