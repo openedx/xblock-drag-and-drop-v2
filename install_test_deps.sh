759668971ae1ba96741335620bde7efc71020924
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2021-12-29 18:13:09 +0500

    fix: try

--- install_test_deps.sh
+++ install_test_deps.sh
@@ -2,5 +2,5 @@
 # Run this script inside a fresh virtual environment.
 rm -rf src/xblock-sdk
 pip install -e git://github.com/edx/xblock-sdk.git@4d0027a040aec1be1a22dc9a4b8fc667bba0ddba#egg=xblock-sdk
-cd src/xblock-sdk/ && ls -la && pip install -r requirements/base.txt && pip install -r requirements/test.txt && cd -
+cd src/xblock-sdk/ && ls -la && pip install -r requirements/base.txt && pip install -r requirements/test.txt && cd - && ls -la
 pip install -r requirements.txt
