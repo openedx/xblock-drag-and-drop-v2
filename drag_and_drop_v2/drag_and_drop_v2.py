a98c61c9dd39bafdc727177d01e540e65e56140c
Agrendalath <piotr@surowiec.it>
2022-10-13 22:18:13 +0200

    fixup! fix: isPastDue inverse logic and add missing attempted_no_past_due option

--- drag_and_drop_v2/drag_and_drop_v2.py
+++ drag_and_drop_v2/drag_and_drop_v2.py
@@ -388,6 +388,7 @@ class DragAndDropBlock(
                 else:
                     item['expandedImageURL'] = ''
             return items
+
         return {
             "block_id": six.text_type(self.scope_ids.usage_id),
             "display_name": self.display_name,
