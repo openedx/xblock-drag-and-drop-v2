0207936c1254dc7e82af8de62a442ecaf9973488
Agrendalath <piotr@surowiec.it>
2023-02-01 12:04:43 +0100

    build: update GitHub workflows

--- setup.py
+++ setup.py
@@ -34,5 +34,5 @@ setup(
     entry_points={
         'xblock.v1': 'drag-and-drop-v2 = drag_and_drop_v2:DragAndDropBlock',
     },
-    package_data=package_data("drag_and_drop_v2", ["static", "templates", "public", "translations", "locale"]),
+    package_data=package_data("drag_and_drop_v2", ["templates", "public", "translations", "locale"]),
 )
