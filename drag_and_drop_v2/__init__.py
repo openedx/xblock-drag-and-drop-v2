8f45989b7169a16e848963d4712a2799d4b1888e
Agrendalath <piotr@surowiec.it>
2023-02-01 13:48:49 +0100

    build: move package version to __init__.py
    This also adds the README and Changelog to PyPi description.

--- drag_and_drop_v2/__init__.py
+++ drag_and_drop_v2/__init__.py
@@ -1,2 +1,4 @@
 """ Drag and Drop v2 XBlock """
 from .drag_and_drop_v2 import DragAndDropBlock
+
+__version__ = "3.1.1"
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 
 import os
 import re
+import sys
 
 from setuptools import find_packages, setup
 
@@ -71,10 +72,38 @@ def is_requirement(line):
     return line and line.strip() and not line.startswith(("-r", "#", "-e", "git+", "-c"))
 
 
+def get_version(*file_paths):
+    """
+    Extract the version string from the file.
+    Input:
+     - file_paths: relative path fragments to file with
+                   version string
+    """
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename, encoding="utf8").read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+VERSION = get_version('drag_and_drop_v2', '__init__.py')
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on GitHub:")
+    os.system("git tag -a %s -m 'version %s'" % (VERSION, VERSION))
+    os.system("git push --tags")
+    sys.exit()
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding="utf8").read()
+CHANGELOG = open(os.path.join(os.path.dirname(__file__), 'Changelog.md'), encoding="utf8").read()
+
 setup(
     name='xblock-drag-and-drop-v2',
-    version='3.1.0',
+    version=VERSION,
     description='XBlock - Drag-and-Drop v2',
+    long_description=README + '\n\n' + CHANGELOG,
+    long_description_content_type='text/markdown',
     install_requires=load_requirements('requirements/base.in'),
     entry_points={
         'xblock.v1': 'drag-and-drop-v2 = drag_and_drop_v2:DragAndDropBlock',
@@ -84,4 +113,5 @@ setup(
         include=['drag_and_drop_v2', 'drag_and_drop_v2.*'],
         exclude=["*tests"],
     ),
+    python_requires=">=3.8",
 )
