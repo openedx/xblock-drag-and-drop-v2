1dd853308f7b04637333e9a0aea3ba1027b98f87
Agrendalath <piotr@surowiec.it>
2023-02-01 13:42:11 +0100

    build: use native function for finding packages

--- setup.py
+++ setup.py
@@ -5,20 +5,7 @@
 import os
 import re
 
-from setuptools import setup
-
-
-# Functions #########################################################
-
-def package_data(pkg, root_list):
-    """Generic function to find package_data for `pkg` under `root`."""
-    data = []
-    for root in root_list:
-        for dirname, _, files in os.walk(os.path.join(pkg, root)):
-            for fname in files:
-                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
-
-    return {pkg: data}
+from setuptools import find_packages, setup
 
 
 def load_requirements(*requirements_paths):
@@ -88,10 +75,13 @@ setup(
     name='xblock-drag-and-drop-v2',
     version='3.1.0',
     description='XBlock - Drag-and-Drop v2',
-    packages=['drag_and_drop_v2'],
     install_requires=load_requirements('requirements/base.in'),
     entry_points={
         'xblock.v1': 'drag-and-drop-v2 = drag_and_drop_v2:DragAndDropBlock',
     },
-    package_data=package_data("drag_and_drop_v2", ["templates", "public", "translations", "locale"]),
+    include_package_data=True,
+    packages=find_packages(
+        include=['drag_and_drop_v2', 'drag_and_drop_v2.*'],
+        exclude=["*tests"],
+    ),
 )
